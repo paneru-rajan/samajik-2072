9960c8634790b5903fb85718c562d03615d9c5d9
d1was <diwaskc12@gmail>
2018-03-22 05:37:35 -0700

    Fibonacci numbers

---
+++ Fibonacci/Fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+        public static void main(String[] args)
+        {
+            int k=0,a=1,b=1;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number upto you want to print:");
+            int n= sc.nextInt();
+            System.out.print("1 1 ");
+            while(k<=n)
+            {
+                k=a+b;
+                if(k>=n)
+                {
+                    break;
+                }
+                System.out.print(k + " " );
+                a=b;
+                b=k;
+
+            }
+        }
+}
