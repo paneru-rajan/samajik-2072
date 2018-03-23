f340a6c51e06c533d7d83611617c780342575edc
anupshrestha11 <anupshrestha11>
2018-03-23 21:40:11 +0545

    project folders

--- Reverse_String/src/ReverseString.java
+++ Reverse_String/src/ReverseString.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+public class ReverseString
+{
+    public static void main(String[] args)
+    {        System.out.print("Enter string to reverse:");
 
-public class ReverseString {
+        Scanner read = new Scanner(System.in);
+        String str = read.nextLine();
+
+        StringBuilder sb = new StringBuilder(str);
+
+        System.out.println("Reversed string is:");
+        System.out.println(sb.reverse().toString());
+        read.close();
+
+    }
 }
