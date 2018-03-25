52ccabd2c721bc3c460d11854ec2884323221947
sandeshp <gafadisandesh@gmail.com>
2018-03-25 22:43:25 +0545

    ReverseString

---
+++ ReverseString/src/ReverseString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args)
+    {
+        //1: Using our own logic
+        String original; //for storing user input
+        String reverse = " "; //for storing reverse
+        //input from user
+        System.out.println("Enter String to Reverse: ");
+        Scanner sc = new Scanner(System.in);
+        original = sc.nextLine();
+        int len = original.length();
+        for (int i= len-1; i>=0;i--)
+            reverse = reverse + original.charAt(i);
+        //Print both the string object
+        System.out.println("Original String is: "+original);
+        System.out.println("Reverse String is: "+reverse);
+
+        /*
+        using string buffer
+        StringBuffer sb = new StringBuffer.(original);
+        reverse = sb.reverse().toString();
+        */
+
+    }
+}
