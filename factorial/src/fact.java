f4eb0d5c04d24e9c0d60c1e59d0a721b85c70939
suraz_neupane <suraznep@gmail.com>
2018-03-25 11:22:35 +0545

    Factorialrecursion

---
+++ factorial/src/fact.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class fact {
+
+
+
+    protected static int factor(int n)
+    {
+        if(n==1)
+            return 1;
+        else
+            return factor(n-1)*n;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number");
+        Scanner scan=new Scanner(System.in);
+        int range=scan.nextInt();
+        System.out.println("The factorial is :"+factor(range));
+    }
+}
