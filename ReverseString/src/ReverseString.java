7bff8878aa7d8842ff24b5ed4b5d54ef541bcd9f
Rajan Paneru <rajan.paneru@yipl.com.np>
2018-03-25 23:01:10 +0545

    ReverseString Solution

---
+++ ReverseString/src/ReverseString.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.print("Enter string to be reversed: ");
+        Scanner read = new Scanner(System.in);
+        String saveLine = read.nextLine();
+        read.close();
+        System.out.println("Reversed string is: " + new StringBuilder(saveLine).reverse());
+    }
+}
