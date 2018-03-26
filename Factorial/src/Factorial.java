ca9e15ad3a97a0dc0f721bd865347c8cac01dfdf
anupshrestha11 <anupshrestha11>
2018-03-26 19:30:23 +0545

    Factorial

--- Factorial/src/Factorial.java
+++ Factorial/src/Factorial.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class Factorial {
     private static int fact(int n){
-        if (n==0)
-            return 1;
-        else if(n>1)
+        if(n>1)
             return n*fact(n-1);
         return 1;
     }
