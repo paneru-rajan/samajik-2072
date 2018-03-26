77b8f34833478503213c50366f5f59fddf2a0e6e
prabesh00 <prabesh.lamichhane1@gmail.com>
2018-03-26 08:34:13 +0545

    fibonocci

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,23 @@
-class HelloWorld{
-    public static void main(String[] args) {
-        System.out.printf("HelloWorld");
+import java.util.Scanner;
+
+
+public class fibo
+{
+    public static int fib(int n) {
+        if(n <= 1) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
     }
+    public static void main(String args[]) {
+        int fib1;
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        fib1 = in.nextInt();
+        int n=fib(fib1);
+        System.out.print("The fibonacci number is: ");
+        System.out.println(n);
+    }
+
 }
