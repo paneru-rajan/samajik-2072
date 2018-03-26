315476015a0ae748accdd94d8d1036be63ca3c59
anupshrestha11 <anupshrestha11>
2018-03-26 19:26:58 +0545

    Factorial

--- Factorial/src/Factorial.java
+++ Factorial/src/Factorial.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Factorial {
-    private static int Fact(int n){
-        if (n==1)
+    private static int fact(int n){
+        if (n==0)
             return 1;
         else if(n>1)
-            return n*Fact(n-1);
-        return 0;
+            return n*fact(n-1);
+        return 1;
     }
     public static void main(String[] args){
         Scanner sr = new Scanner(System.in);
         System.out.print("\tProgram to find Factorial\n\n");
         System.out.print("Enter the number find Factorial: ");
         int f = sr.nextInt();
-        System.out.print("\nThe Factorial of "+f+" is: "+Fact(f));
+        System.out.print("\nThe Factorial of "+f+" is: "+fact(f));
     }
 }
