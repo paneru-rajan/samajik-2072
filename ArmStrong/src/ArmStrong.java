cd1439738cf8db0345533582979896e61f947c45
anupshrestha11 <anupshrestha11>
2018-03-26 21:59:37 +0545

    Armstrong modified

--- ArmStrong/src/ArmStrong.java
+++ ArmStrong/src/ArmStrong.java
@@ -1,16 +1,24 @@
+import static java.lang.Math.pow;
 public class ArmStrong {
-    public static void main(String[] args)  {
-        int a,temp,b;
-        for(int i = 100;i<999;i++){
-            temp=i;
-            a=temp%10;
-            temp/=10;
-            b=temp%10;
-            temp/=10;
-            temp=(a*a*a)+(b*b*b)+(temp*temp*temp);
-            if(i==temp)
-                System.out.print(temp+", ");
+    static int conc(int i){
+        int b=0, a;
+        while (i > 0)
+        {
+            a = i % 10;
+            b = b + (int) pow(a,3);
+            i /= 10;
         }
-
+        return b;
+    }
+    public static void main(String[] args)  {
+        int  b;
+        System.out.print("\n\tProgram to generate Armstrong numbers:\n");
+        System.out.print("\nThe Armstrong numbers are (of 3 digits): ");
+        for(int i = 100;i<999;i++){
+            b=conc(i);
+            if(b==i)
+                System.out.print(b+", ");
+        }
+        System.out.println();
     }
 }
