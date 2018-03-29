dacc38a9ddc5af9fa6b5a9c1dee58bd65ea92c1a
prabesh00 <prabesh.lamichhane1@gmail.com>
2018-03-30 03:41:00 +0545

    prime

---
+++ prime/src/prime.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class prime
+{
+    public static void main(String[] args)
+    {
+        int i,num,j;
+        boolean flag=false;
+        System.out.println("Enter a number ");
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+        double l=Math.sqrt(num);
+        j=(int)l;
+
+        for(i=2;i<j;i++)
+        {
+            if(j%i==0)
+            {
+               flag=true; break;
+            }
+            else
+                continue;
+
+
+        }
+        if(flag==true)
+            System.out.println("The number is not prime");
+        else
+            System.out.println("The number is prime");
+    }
+}
