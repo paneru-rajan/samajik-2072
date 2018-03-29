bd560c8f2da7b19e1e5f6f4a7713977b0d2c1b96
Ayush Acharya <aus.acharya@gmail.com>
2018-03-29 19:51:55 +0545

    Factorial

---
+++ Factorial/src/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    static int fact(int n) {
+        if(n>1)
+            return n*(n-1);
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Factorial of what?");
+        Scanner read = new Scanner(System.in);
+        n = read.nextInt();
+        read.close();
+        System.out.println(fact(n));
+    }
+}
